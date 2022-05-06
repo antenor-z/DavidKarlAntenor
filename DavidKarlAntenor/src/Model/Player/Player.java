309791bbb0dfcde82b2fdc0d8e3b9e2d8b330851
arm <arm@oxberry>
2022-05-06 15:30:08 -0300

    add deposit and withdraw methods

--- DavidKarlAntenor/src/Model/Player/Player.java
+++ DavidKarlAntenor/src/Model/Player/Player.java
@@ -3,7 +3,7 @@ package Model.Player;
 import Model.gameboard.Tile;
 
 public class Player {
-	int cash;
+	private int cash;
 	Tile currentTile;
 	public Player(int cash, Tile initialTile) {
 		this.cash = cash;
@@ -11,5 +11,20 @@ public class Player {
 	}
 	public void advance(int n) {
 		
+	}
+	public void deposit(int ammount)
+	{
+		cash += ammount;
+	}
+	public void withdraw(int ammount) throws PlayerException
+	{
+		if(cash - ammount > 0)
+		{
+			cash -= ammount;
+		}
+		else
+		{
+			throw new PlayerException("Cash less than zero");
+		}
 	}
 }
