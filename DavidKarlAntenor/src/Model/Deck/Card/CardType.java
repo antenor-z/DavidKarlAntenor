ab5dd0f13234307ab191a0b49ac0cd72e946ebc9
karlvignon <karl.vignon@epitech.eu>
2022-05-06 13:45:16 -0300

    deck.json ok and create MOVE/VALUE_PER_PLAYER cardtypes

--- DavidKarlAntenor/src/Model/Deck/Card/CardType.java
+++ DavidKarlAntenor/src/Model/Deck/Card/CardType.java
@@ -3,13 +3,17 @@ package Model.Deck.Card;
 public enum CardType {
     VALUE,
     GO_TO_JAIL,
-    OUT_OF_JAIL;
+    OUT_OF_JAIL,
+    VALUE_PER_PLAYER,
+    MOVE;
 
     public static CardType fromInteger(int x) {
         return switch (x) {
             case 0 -> VALUE;
             case 1 -> GO_TO_JAIL;
             case 2 -> OUT_OF_JAIL;
+            case 3 -> VALUE_PER_PLAYER;
+            case 4 -> MOVE;
             default -> null;
         };
     }
