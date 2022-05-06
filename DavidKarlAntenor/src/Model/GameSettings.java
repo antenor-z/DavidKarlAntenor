ebe53d4f0d78ce5cca32353a7c38473b0cb366e6
karlvignon <karl.vignon@epitech.eu>
2022-05-06 14:03:47 -0300

    add GameSettings singleton

---
+++ DavidKarlAntenor/src/Model/GameSettings.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class GameSettings {
+    static GameSettings instance;
+
+    private Integer maxPlayer = 6;
+    private Integer minPlayer = 2;
+    private Float maxDuration = -1.f; // set to -1 if there is no max duration
+    private Integer startingBalance = 200;
+    private Integer nbDices = 2;
+
+    public GameSettings() {
+    }
+
+    public static GameSettings getInstance() {
+        if (instance == null) {
+            instance = new GameSettings();
+        }
+        return instance;
+    }
+}
