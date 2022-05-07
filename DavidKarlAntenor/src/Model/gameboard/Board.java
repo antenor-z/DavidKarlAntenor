3a72c463c65f3a024cd075f196617423ef882cb8
arm <arm@stargate>
2022-05-07 20:12:20 -0300

    get the information to construct classes from Board.json

--- DavidKarlAntenor/src/Model/gameboard/Board.java
+++ DavidKarlAntenor/src/Model/gameboard/Board.java
@@ -1,18 +1,81 @@
 package Model.gameboard;
 import org.json.*;
-import Model.Utils.Files;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Board {
 	private Tile[] tiles;
-	private final int numberOfTiles = 40;
 	public Board()
-	{
-		String jsonString = Files.getFileContent("Board.json");
-		JSONObject obj = new JSONObject(jsonString);
-		JSONArray jsonTiles = obj.getJSONArray("tiles");
+	{	
+		tiles = new Tile[40];
+		try
+		{
+			String content = Files.readString(Path.of("./Board.json"));
+			JSONObject obj = new JSONObject(content);
+            JSONArray jsonBoard = obj.getJSONArray("board");
+            
+            for (Object jsonTile: jsonBoard) {
+                
+            	JSONObject jsonTileObj = (JSONObject)jsonTile;
+            	String type = jsonTileObj.getString("type");
+            	int position = jsonTileObj.getInt("position");
+            	if(type.equals("Land"))
+            	{  		
+            		String description = jsonTileObj.getString("description");
+            		int price = jsonTileObj.getInt("price");
+            		int buildHouseCost = jsonTileObj.getInt("buildHouseCost");
+            		int buildHotelCost = jsonTileObj.getInt("buildHotelCost");
+            		JSONArray jsonRentCost = jsonTileObj.getJSONArray("rentCost");
+            		int rentCost[] = new int[6];
+            		for(int i = 0; i < 6; i++)
+            		{
+            			rentCost[i] = jsonRentCost.getInt(i);
+            		}
+            		//tiles[position] = new Land(description, price, buildHouseCost, buildHotelCost, rentCost);
+            	}
+            	else if(type.equals("Company"))
+            	{
+            		String description = jsonTileObj.getString("description");
+            		int price = jsonTileObj.getInt("price");
+            		int priceRate = jsonTileObj.getInt("priceRate");
+            		tiles[position] = new Company(description, price, priceRate);
+            	}
+            	else if(type.equals("prision"))
+            	{
+            		tiles[position] = new Prision();
+            	}
+            	else if(type.equals("LuckSetBack"))
+            	{
+            		tiles[position] = new Lucksetback();
+            	}
+            	else if(type.equals("Money"))
+            	{
+            		int ammount = jsonTileObj.getInt("ammount");
+            		tiles[position] = new Money(ammount);
+            	}
+            	else if(type.equals("Freestop"))
+            	{
+            		tiles[position] = new Freestop();
+            	}
+            	else if(type.equals("GoToPrision"))
+            	{
+            		tiles[position] = new GoToPrision();
+            	}
+            	else if(type.equals("Start"))
+            	{
+            		tiles[position] = new Start();
+            	}
+          
+            }
+           
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		
 	}
 	public Tile[] getTiles(){
 		return tiles;
