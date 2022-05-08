f8692172e87b0dd2179502cab6843ad5460b4f5d
arm <arm@stargate>
2022-05-07 21:01:27 -0300

    player test

--- DavidKarlAntenor/src/Model/Player/PlayerTest.java
+++ DavidKarlAntenor/src/Model/Player/PlayerTest.java
@@ -1,163 +1,70 @@
-import org.junit.*;
+package Model.Player;
+
+import Model.gameboard.Board;
 import static org.junit.Assert.*;
 import org.junit.Test;
-import java.util.*;
 
 public class PlayerTest {
 
 	/*
 		---------------------------------------------------------------------
-		|Player - TestClass - Balance													
+		|Player - TestClass - ChangeCash													
 		|Testing the player's balance:										
-		|Testcases-> Player's balance null.									
-		|			 Player's balance is not an integer.						
+		|Testcases-> Player's balance equal to 4000.									
+		|			 Player's balance equal to 3000.
+		|			 Player's balance is positive						
 		---------------------------------------------------------------------
 	*/
 	@Test
-	public void testBalance() {
-		
-		int balance = 500;
-		assertFalse("Player's balance can't be less than 0", balance < 0);
+	public void testChangeCash() {
+		Board b = new Board();
+		Player p = new Player(4000, b);
+		assertEquals("Player's balance needs to be 4000", p.getCash(), 4000);
 		try {
-			Integer.valueOf(balance);
-			
-		}catch (NumberFormatException e) {
-			fail("Fail: 01 -> Player's balance should be an integer value");
-		
+		p.changeCash(1000);
+		} catch (PlayerException e)
+		{
+			fail(e.getMessage());
+		}
+		assertEquals("Player's balance needs to be 3000", p.getCash(), 5000);
+		try {
+		p.changeCash(-5000);
+		} catch (PlayerException e)
+		{
+			fail(e.getMessage());
+		}
+		assertEquals("Player's balance needs to be 0", p.getCash(), 0);
+		try {
+			p.changeCash(-1);
+			fail("Fail: 01 -> Player's balance should be postive");
+		}catch (PlayerException e) {
+
 		}		
-		
-		
 	}
+	
 	/*
-		---------------------------------------------------------------------
-		|Player - TestClass - Territories													
-		|Testing the territories owned by player (assuming they will be 	
-		|identified by integers):											
-		|Testcases-> Player's territory is numbered by 0.								
-		|			 Player's territory is numbered by a value less than 0.		
-		|			 Player's territory is numbered by a value higher than
-		|the amount of territories in he game.	
-		---------------------------------------------------------------------
-
-	*/
+	---------------------------------------------------------------------
+	|Player - TestClass - Advance													
+	|Testing the advance method:										
+	|Testcases-> Player can move +3.									
+	|			 Player cannot move -3.
+	|			 Player can move +7
+	|		     Player is in position 10 now
+	|		     Player can move +31
+	|			 Player is in position 1					
+	---------------------------------------------------------------------
+*/
 	@Test
-	public void testTerritories() {
-		List<Integer> territories = new ArrayList<Integer>();
-		territories.add(-1);
-		territories.add(-2);
-		territories.add(0);
-		territories.add(1);
-		territories.add(2);
-		territories.add(4);
-		territories.add(13);
-		territories.add(17);
-		territories.add(18);
-		territories.add(19);
-		territories.add(20);
-		
-		assertFalse("Fail: 02 ->  Territores must not contain the value 0", territories.get(0) == 0);
-		assertFalse("Fail: 03 ->  Territores must not contain the value 0", territories.get(1) == 0);
-		assertFalse("Fail: 04->  Territores must not contain the value 0", territories.get(2) == 0);
-		assertFalse("Fail: 05->  Territores must not contain the value 0", territories.get(3) == 0);
-		assertFalse("Fail: 06 ->  Territores must not contain the value 0", territories.get(4) == 0);
-		assertFalse("Fail: 07 ->  Territores must not contain the value 0", territories.get(0) < 0);
-		assertFalse("Fail: 03 ->  Territores must not contain the value 0", territories.get(1) < 0);
-		assertFalse("Fail: 04->  Territores must not contain the value 0", territories.get(2) < 0);
-		assertFalse("Fail: 05->  Territores must not contain the value 0", territories.get(3) < 0);
-		assertFalse("Fail: 06 ->  Territores must not contain the value 0", territories.get(4) <0);
-		assertTrue("Fail: 02 ->  Territores must not contain the value 0", territories.get(5) > 0);
-		assertTrue("Fail: 03 ->  Territores must not contain the value 0", territories.get(6) > 0);
-		assertTrue("Fail: 04->  Territores must not contain the value 0", territories.get(7) > 0);
-		assertTrue("Fail: 05->  Territores must not contain the value 0", territories.get(8) > 0);
-		assertFalse("Fail -> Value higher than the amount of territories", territories.get(6) > 18);
-		assertFalse("Fail -> Value higher than the amount of territories", territories.get(7) > 18);
-		assertFalse("Fail -> Value higher than the amount of territories", territories.get(8) > 18);
-		assertFalse("Fail -> Value higher than the amount of territories", territories.get(9) > 18);
-		assertFalse("Fail -> Value higher than the amount of territories", territories.get(10) > 18);
-		
+	public void testAdvance() {
+		Board b = new Board();
+		Player p = new Player(4000, b);
+		assertTrue("You can advance positive", p.advance(3));
+		assertFalse("You can advance positive", p.advance(-3));
+		assertTrue("You can advance positive", p.advance(7));
+		assertEquals("The game must be at position 10 now", p.getCurrentTile(), 10);
+		assertTrue("You can advance positive", p.advance(31));
+		assertEquals("The position must roll back to 1", p.getCurrentTile(), 1);
 	}
-	/*
-		---------------------------------------------------------------------
-		|Player - TestClass	- Companies												
-		|Testing the territories owned by player (assuming they will be 	
-		|identified by integers):											
-		|Testcases-> Player's copmany is numbered by 0.								
-		|			 Player's company is numbered by a value less than 0.		\
-		|			 Player's copmany is numbered by a value higher than
-		|the amount of companies in the game.	
-		---------------------------------------------------------------------
-
-	*/
-	@Test
-	public void testCompanies() {
-		List<Integer> companies = new ArrayList<Integer>();
-		companies.add(-2);
-		companies.add(-1);
-		companies.add(0);
-		companies.add(1);
-		companies.add(5);
-		companies.add(6);
-		companies.add(7);
-		assertFalse("Fail: 02 ->  Territores must not contain the value 0", companies.get(0) == 0);
-		assertFalse("Fail: 03 ->  Territores must not contain the value 0", companies.get(1) == 0);
-		assertFalse("Fail: 04->  Territores must not contain the value 0", companies.get(2) == 0);
-		assertFalse("Fail: 05->  Territores must not contain the value 0", companies.get(3) == 0);
-		assertFalse("Fail: 06 ->  Territores must not contain the value 0", companies.get(4) == 0);
-		assertFalse("Fail: 07 ->  Territores must not contain the value 0", companies.get(0) < 0);
-		assertFalse("Fail: 03 ->  Territores must not contain the value 0", companies.get(1) < 0);
-		assertFalse("Fail: 04->  Territores must not contain the value 0", companies.get(2) < 0);
-		assertFalse("Fail: 05->  Territores must not contain the value 0", companies.get(3) < 0);
-		assertFalse("Fail: 06 ->  Territores must not contain the value 0", companies.get(4) <0);
-		assertTrue("Fail: 02 ->  Territores must not contain the value 0", companies.get(5) > 0);
-		assertTrue("Fail: 03 ->  Territores must not contain the value 0", companies.get(6) > 0);
-		assertTrue("Fail: 04->  Territores must not contain the value 0", companies.get(7) > 0);
-		assertTrue("Fail: 05->  Territores must not contain the value 0", companies.get(8) > 0);
-		assertFalse("Fail -> Value higher than the amount of companies", companies.get(6) > 18);
-		assertFalse("Fail -> Value higher than the amount of companies", companies.get(7) > 18);
-		assertFalse("Fail -> Value higher than the amount of companies", companies.get(8) > 18);
-		
-	}
-	/*
-		---------------------------------------------------------------------
-		|Player - TestClass	- Deposit												
-		|Testing the territories owned by player (assuming they will be 	
-		|identified by integers):											
-		|Testcases-> Player's deposit contains value 0.								
-		|			 Player's deposit contains value less than 0.	
-		|			 Player's deposit contains value NULL.		
-		|			
-		---------------------------------------------------------------------
-
-	*/
-	@Test
-	public void testDeposit(){
-		int amount = 200;
-		assertEquals(0, amount, "Fail: 26 -> Deposits cannot contain the value 0");
-		assertNotNull(amount, "Fail: 27 -> Deposits cannot contain the value NULL");
-		assertFalse(amount < 0, "Fail: 28 -> Deposits cannot contain values less than 0");
-
-	}
-	/*
-		---------------------------------------------------------------------
-		|Player - TestClass	- Deposit												
-		|Testing the territories owned by player (assuming they will be 	
-		|identified by integers):											
-		|Testcases-> Player's withdrawn contains value 0.		
-		|			 Player's withdrawn contains value NULL.						
-		|			 Player's withdrawn contains value less than 0.	
-		|			 		
-		|			
-		---------------------------------------------------------------------
-
-	*/
-	@Test
-	public void testWithdraw(){
-		int amount = 300;
-		assertNotEqual(0, amount, "Fail: 29 -> Withdrawns cannot contain the value 0");
-		assertNotNull(amount, "Fail: 30 -> Withdrawns cannot contain the value NULL");
-		assertFalse(amount < 0, "Fail: 31 -> Withdrawns cannot contain values less than 0");
-	}
-
-
+	
 
 }
