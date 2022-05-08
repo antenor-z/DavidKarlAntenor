9ff91a773d4d27f8ef753745cd2cf37f7b323153
arm <arm@stargate>
2022-05-07 21:33:12 -0300

    dice test

---
+++ DavidKarlAntenor/src/Model/Dice/DiceTest.java
@@ -0,0 +1,19 @@
+package Model.Dice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiceTest {
+
+	@Test
+	public void testRoll() {
+		assertTrue("Must be less than 6", Dice.roll()[0] < 6); 
+		assertTrue("Must be less than 6", Dice.roll()[0] < 6); 
+		assertTrue("Must be less than 6", Dice.roll()[0] < 6); 
+		assertTrue("Must be less than 6", Dice.roll()[1] < 6); 
+		assertTrue("Must be less than 6", Dice.roll()[1] < 6); 
+		assertTrue("Must be less than 6", Dice.roll()[1] < 6); 
+	}
+
+}
