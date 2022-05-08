77005515844e6cbc3aab9e3957a866d904077636
arm <arm@stargate>
2022-05-08 19:27:39 -0300

    fix

--- DavidKarlAntenor/src/Model/gameboard/LandTest.java
+++ DavidKarlAntenor/src/Model/gameboard/LandTest.java
@@ -3,20 +3,24 @@ package Model.gameboard;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+import Model.Player.Player;
+import Model.Player.PlayerException;
+
 import java.util.*;
 
 public class LandTest{
   @Test
-	public void testBuyLand() {
+	public void testBuyLand() throws LandException {
 		Board b = new Board();
 		Player p = new Player(4000, b);
 		Player p1 = new Player(4000, b);
-		Land l = b.getTile(6);
-		assertTrue("Position 6 not buyable land", l instanceof Company);
+		assertTrue("Position 6 not buyable land", b.getTile(6) instanceof Company);
+		Land l = (Land)b.getTile(6);
 		l.buyLand(p);
 		try {
 			l.buyLand(p1);
-			fail("Fail -> Land already owned by other player.")
+			fail("Fail -> Land already owned by other player.");
 		}
 		catch(LandException e)
 		{
@@ -24,12 +28,12 @@ public class LandTest{
 		}
 	}
   @Test 
-	public void testRent() {
+	public void testRent() throws LandException, PlayerException {
 		Board b = new Board();
 		Player p = new Player(4000, b);
 		Player p1 = new Player(4000, b);
-		Land l = b.getTile(6);//Rent = 20 - 1 House = 100 - 2 House = 300 - 3 House = 750 - 4 House = 925 - Hotel = 1100
-		Land l1 = b.getTile(1);//Rent = 6 - 1 House = 30 - 2 House = 90 - 3 House = 270 - 4 House = 400 - Hotel = 500
+		Land l = (Land) b.getTile(6);//Rent = 20 - 1 House = 100 - 2 House = 300 - 3 House = 750 - 4 House = 925 - Hotel = 1100
+		Land l1 = (Land) b.getTile(1);//Rent = 6 - 1 House = 30 - 2 House = 90 - 3 House = 270 - 4 House = 400 - Hotel = 500
 		try {
 			
 			l.payRent(p);
@@ -55,30 +59,46 @@ public class LandTest{
 		catch(LandException e){
 			System.out.print(e.getMessage());
 		}
+		int p1Cash = 4000 - 20;
+		int pCash = 4000 + 20;
 		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), 4000 - 20);
-		assertEquals("Player p should have received rent.", p.getCash(), 4000 + 20);
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
 		l.buildHouse(p);
+		pCash -= 50;
 		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), (4000 - 20) - 100);
-		assertEquals("Player p should have received rent.", p.getCash(),(4000 + 20) + 100);
-		l.buildHose(p);
-		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), (4000 - 20 - 100) - 300);
-		assertEquals("Player p should have received rent.", p.getCash(),(4000 + 20 + 100) + 300);
-		l.buildHose(p);
-		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), (4000 - 20 - 100 - 300) - 750);
-		assertEquals("Player p should have received rent.", p.getCash(),(4000 + 20 + 100 + 300) + 750);
+		pCash += 100;
+		p1Cash -= 100;
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
 		l.buildHouse(p);
+		pCash -= 50;
 		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), (4000 - 20 - 100 - 300 - 750) - 925);
-		assertEquals("Player p should have received rent.", p.getCash(),(4000 + 20 + 100 + 300 +750) + 925);
+		pCash += 300;
+		p1Cash -= 300;
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
+		l.buildHouse(p);
+		pCash -= 50;
+		l.payRent(p1);
+		pCash += 750;
+		p1Cash -= 750;
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
+		l.buildHouse(p);
+		pCash -= 50;
+		l.payRent(p1);
+		pCash += 925;
+		p1Cash -= 925;
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
 		l.buildHotel(p);
+		pCash -= 50;
 		l.payRent(p1);
-		assertEquals("Player p1 should have paid rent.", p1.getCash(), (4000 - 20 - 100 - 300 - 750 - 925) - 1100);
-		assertEquals("Player p should have received rent.", p.getCash(),(4000 + 20 + 100 + 300 +750 + 925) + 1100);
-		
+		pCash += 1100;
+		p1Cash -= 1100;
+		assertEquals("Player p1 should have paid rent.", p1.getCash(), p1Cash);
+		assertEquals("Player p should have received rent.", p.getCash(), pCash);
 		
 	}		
 }
