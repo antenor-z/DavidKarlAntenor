428850745b71ba08a0e03909f73ad1481d684374
arm <arm@stargate>
2022-05-11 16:25:03 -0300

    more tests

--- DavidKarlAntenor/src/Model/gameboard/CompanyTest.java
+++ DavidKarlAntenor/src/Model/gameboard/CompanyTest.java
@@ -12,15 +12,16 @@ public class CompanyTest {
 	@Test
 	public void testBuyCompany() throws CompanyException {
 		Board b = new Board();
-		Player p = new Player(4000, b, PlayerColor.BLUE);
-		Player q = new Player(4000, b, PlayerColor.ORANGE);
+		Player blue = new Player(4000, b, PlayerColor.BLUE);
+		Player orange = new Player(4000, b, PlayerColor.ORANGE);
 		assertTrue("Position 5 not company", b.getTile(5) instanceof Company);
 		Company c = (Company)b.getTile(5);
-		c.buyCompany(p);
+		c.buyCompany(blue);
+		assertEquals("Blue should be the owner", c.getOwner(), blue);
 		try
 		{
-			c.buyCompany(q);
-			fail("Company is owned by p");
+			c.buyCompany(orange);
+			fail("Company is owned by blue");
 		}
 		catch(CompanyException e)
 		{
@@ -31,14 +32,27 @@ public class CompanyTest {
 	@Test
 	public void testPayRent() throws CompanyException {
 		Board b = new Board();
-		Player p = new Player(4000, b, PlayerColor.BLUE);
-		Player q = new Player(4000, b, PlayerColor.ORANGE);
+		Player blue = new Player(4000, b, PlayerColor.BLUE);
+		Player orange = new Player(4000, b, PlayerColor.ORANGE);
 		Company c = (Company)b.getTile(5);
-		c.buyCompany(p);
-		c.payRent(q, 4);
-		assertEquals("p should have received rent", p.getCash(), 4000+200);
-		assertEquals("q should have payed rent", q.getCash(), 4000-200);
-		
+		c.buyCompany(orange);
+		c.payRent(blue, 4);
+		assertEquals("orange should have received rent", c.getOwner().getCash(), 4000+200);
+		assertEquals("blue should have payed rent", blue.getCash(), 4000-200);
+		try
+		{
+			c.payRent(c.getOwner(), 4);
+			fail("Can't pay rent to themself.");
+		}
+		catch(CompanyException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		Company d = (Company)b.getTile(15);
+		d.buyCompany(blue);
+		d.payRent(orange, 6);
+		assertEquals("blue should have received rent", d.getOwner().getCash(), 3800 + 240);
+		assertEquals("orange should have payed rent", orange.getCash(), 4200 - 240);
 	}
 
 }
