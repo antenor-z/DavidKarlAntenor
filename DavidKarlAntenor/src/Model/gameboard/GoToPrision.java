7aef5151a307084d795478efed7cc6f48faaee66
arm <arm@stargate>
2022-05-11 15:57:08 -0300

    goToPrision sends player to prision tile

--- DavidKarlAntenor/src/Model/gameboard/GoToPrision.java
+++ DavidKarlAntenor/src/Model/gameboard/GoToPrision.java
@@ -1,7 +1,12 @@
 package Model.gameboard;
 
+import Model.Player.Player;
+
 public class GoToPrision extends Tile{
 	void print(){
 		System.out.println("I'm a GoToPrision tile");
+	}
+	void gotoPrision(Player player) {
+		player.goToTile(10);
 	}
 }
