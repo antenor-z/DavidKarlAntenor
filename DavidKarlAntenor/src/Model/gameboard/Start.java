f42bd1f873ab51e03c8fe02a6706b7f90d39d286
arm <arm@stargate>
2022-05-11 15:57:25 -0300

    Player gets 200 on start tile

--- DavidKarlAntenor/src/Model/gameboard/Start.java
+++ DavidKarlAntenor/src/Model/gameboard/Start.java
@@ -1,7 +1,14 @@
 package Model.gameboard;
 
+import Model.Player.Player;
+import Model.Player.PlayerException;
+
 public class Start extends Tile {
 	void print(){
 		System.out.println("I'm a Start tile");
+	}
+	void execute(Player player) throws PlayerException
+	{
+		player.changeCash(200);
 	}
 }
