2b6dee3e0f52c48689f0160950bb4d1836810057
arm <arm@stargate>
2022-05-12 22:17:24 -0300

    Using enum instead of string

--- DavidKarlAntenor/src/view/Menu.java
+++ DavidKarlAntenor/src/view/Menu.java
@@ -1,17 +1,30 @@
 package view;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 @SuppressWarnings("serial")
 public class Menu extends JFrame {
 	JButton btnNext = new JButton("Next");
 	JButton btnOpen = new JButton("Open saved game");
 	JLabel label = new JLabel("Choose at least 3 colors to play");
 	ArrayList<JCheckBox> checkBoxes = new ArrayList<JCheckBox>();
-	String[] colorNames = {"Red", "Green", "Blue", "Gray", "Purple", "Yellow"};
+	String[] colorNames = {
+			Model.PlayerColor.Blue.toString(), 
+			Model.PlayerColor.Red.toString(),
+			Model.PlayerColor.Orange.toString(),
+			Model.PlayerColor.Gray.toString(),
+			Model.PlayerColor.Purple.toString(),
+			Model.PlayerColor.Yellow.toString(),};
+	HashMap<Model.PlayerColor, String> playersName = new HashMap<Model.PlayerColor, String>();
 	final private int windowSizeX = 450;
 	final private int windowSizeY = 320;
 	public Menu() {	
@@ -21,6 +34,10 @@ public class Menu extends JFrame {
 		insertCheckBoxes();
 		insertLabel();
 		insertButtons();
+		for(JCheckBox chkBox: checkBoxes) {
+			chkBox.addItemListener(new chkStateChanged());
+		}
+		btnNext.addActionListener(new btnClicked());
 	}
 	private void insertLabel() {
 		label.setBounds(20, 40, 500, 30);
@@ -43,6 +60,7 @@ public class Menu extends JFrame {
 		for(int y = 0; y < 2; y++) {
 			positionY = startY + y * sizeY;
 			for(int x = 0; x < 3; x++) {
+				
 				checkBoxes.add(new JCheckBox(colorNames[color]));
 				positionX = startX + x * sizeX;
 				checkBoxes.get(color).setBounds(positionX, positionY, sizeX, sizeY);
@@ -55,5 +73,43 @@ public class Menu extends JFrame {
 		Menu f=new Menu();
 		f.setTitle("Menu");
 		f.setVisible(true);
+	}
+	public class chkStateChanged implements ItemListener {
+		public void itemStateChanged(ItemEvent e) {
+			int count = 0;
+			for(JCheckBox chkBox: checkBoxes) {
+				if(chkBox.isSelected())
+					count++;
+			}
+			if(count >= 3) btnNext.setEnabled(true);
+			else btnNext.setEnabled(false);
+		} 
+	}
+	public class btnClicked implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			for(JCheckBox chkBox: checkBoxes) {
+				if(chkBox.isSelected())
+				{
+					Model.PlayerColor pc = Model.PlayerColor.valueOf(chkBox.getText());
+					String s;
+					while(true)
+					{
+						s = (String)JOptionPane.showInputDialog("Name for player " + chkBox.getText());
+						if ((s.length() == 0) || (s.length() > 8))
+						{
+							JOptionPane.showMessageDialog(null, "Name of player must be between 1 and 8 chars.");
+						}
+						else if(playersName.containsValue(s))
+						{
+							JOptionPane.showMessageDialog(null, "This name was already choosen.");
+						}
+						else break;
+					}
+					playersName.put(pc, s);	
+				}
+			}
+			JOptionPane.showMessageDialog(null, playersName);
+			dispose();
+		} 
 	}
 }
