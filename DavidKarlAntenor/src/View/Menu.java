eaa5dfd56b14447610621510770feec831d1c0ad
arm <arm@stargate>
2022-05-14 19:23:35 -0300

    Put players' name on the menu

--- DavidKarlAntenor/src/View/Menu.java
+++ DavidKarlAntenor/src/View/Menu.java
@@ -4,17 +4,24 @@ import Model.Event.ViewType;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 @SuppressWarnings("serial")
 public class Menu extends GameFrame {
 	JButton btnNext = new JButton("Play");
 	JButton btnOpen = new JButton("Open saved game");
-	JLabel label = new JLabel("Choose at least 3 colors to play");
+	JLabel label = new JLabel("Choose at least 3 colors to play. Add a name for each one.");
+	
 	ArrayList<JCheckBox> checkBoxes = new ArrayList<JCheckBox>();
+	ArrayList<JTextField> textFields = new ArrayList<JTextField>();
+	
 	String[] colorNames = {
 			Model.PlayerColor.Blue.toString(),
 			Model.PlayerColor.Red.toString(),
@@ -35,16 +42,20 @@ public class Menu extends GameFrame {
 		setResizable(false);
 		setLocation((Toolkit.getDefaultToolkit().getScreenSize().width)/2 - getWidth()/2, (Toolkit.getDefaultToolkit().getScreenSize().height)/2 - getHeight()/2);
 		insertCheckBoxes();
-		insertLabel();
+		insertLabels();
 		insertButtons();
+		insertTextFields();
 		for(JCheckBox chkBox: checkBoxes) {
 			chkBox.addItemListener(new chkStateChanged());
+		}
+		for(JTextField txt: textFields) {
+			txt.getDocument().addDocumentListener(new textChanged());
 		}
 		PlayBtnListener playBtnListener = new PlayBtnListener(parent);
 		btnNext.addActionListener(playBtnListener);
 	}
-	private void insertLabel() {
-		label.setBounds(20, 40, 500, 30);
+	private void insertLabels() {
+		label.setBounds(20, 20, 500, 30);
 		getContentPane().add(label);
 	}
 	private void insertButtons() {
@@ -59,14 +70,16 @@ public class Menu extends GameFrame {
 		int color = 0, positionX, positionY;
 		final int startX = 80;
 		final int startY = 80;
-		final int sizeX = 120;
-		final int sizeY = 70;
+		final int sizeX = 70;
+		final int sizeY = 20;
+		final int spacingX = 120;
+		final int spacingY = 70;
 		for(int y = 0; y < 2; y++) {
-			positionY = startY + y * sizeY;
+			positionY = startY + y * spacingY;
 			for(int x = 0; x < 3; x++) {
 
 				checkBoxes.add(new JCheckBox(colorNames[color]));
-				positionX = startX + x * sizeX;
+				positionX = startX + x * spacingX;
 				checkBoxes.get(color).setBounds(positionX, positionY, sizeX, sizeY);
 				getContentPane().add(checkBoxes.get(color));
 				color++;
@@ -74,15 +87,79 @@ public class Menu extends GameFrame {
 		}
 	}
 
+	private void insertTextFields() {
+		int positionX, positionY;
+		final int startX = 80;
+		final int startY = 110;
+		final int sizeX = 70;
+		final int sizeY = 20;
+		final int spacingX = 120;
+		final int spacingY = 70;
+		for(int y = 0; y < 2; y++) {
+			positionY = startY + y * spacingY;
+			for(int x = 0; x < 3; x++) {
+				JTextField txt = new JTextField();
+				textFields.add(txt);
+				positionX = startX + x * spacingX;
+				txt.setBounds(positionX, positionY, sizeX, sizeY);
+				txt.setEnabled(false);
+				getContentPane().add(txt);
+			}
+		}
+	}
+	
+	void validateTextCombo() {
+		int count = 0;
+		boolean textFieldsSizeOK = true;
+		boolean textFieldsAllUnique = true;
+		
+		for(int i = 0; i < 6; i++) {
+			if(checkBoxes.get(i).isSelected()){
+				count++;
+				textFields.get(i).setEnabled(true);
+				String text = textFields.get(i).getText();
+				
+				if(text.length() == 0 || text.length() > 8) {
+					textFieldsSizeOK = false;
+				}
+				else {
+					for(int j = 0; j < 6; j++) {
+						String text2 = textFields.get(j).getText();
+						if(text2.equals(text) && i != j) {
+							textFieldsAllUnique = false;	
+							break;
+						}
+					}
+				}
+			}
+			else {
+				textFields.get(i).setEnabled(false);
+			}
+		}
+	
+		if(count >= 3 && textFieldsSizeOK && textFieldsAllUnique)
+			btnNext.setEnabled(true);
+		else
+			btnNext.setEnabled(false);
+	}
+	public class textChanged implements DocumentListener {
+
+		public void insertUpdate(DocumentEvent e) {
+			validateTextCombo();
+		}
+
+		public void removeUpdate(DocumentEvent e) {
+			validateTextCombo();
+		}
+
+		public void changedUpdate(DocumentEvent e) {
+			validateTextCombo();
+		}
+		
+	}
 	public class chkStateChanged implements ItemListener {
 		public void itemStateChanged(ItemEvent e) {
-			int count = 0;
-			for(JCheckBox chkBox: checkBoxes) {
-				if(chkBox.isSelected())
-					count++;
-			}
-			if(count >= 3) btnNext.setEnabled(true);
-			else btnNext.setEnabled(false);
+			validateTextCombo();
 		}
 	}
 
@@ -94,27 +171,14 @@ public class Menu extends GameFrame {
 		}
 
 		public void actionPerformed(ActionEvent e) {
-			/*for(JCheckBox chkBox: checkBoxes) {
-				if(chkBox.isSelected())
-				{
-					Model.PlayerColor pc = Model.PlayerColor.valueOf(chkBox.getText());
-					String s;
-					while(true)
-					{
-						s = (String)JOptionPane.showInputDialog("Name for player " + chkBox.getText());
-						if (s == null || (s.length() == 0) || (s.length() > 8))
-						{
-							JOptionPane.showMessageDialog(null, "Name of player must be between 1 and 8 chars.");
-						}
-						else if(playersName.containsValue(s))
-						{
-							JOptionPane.showMessageDialog(null, "This name was already choosen.");
-						}
-						else break;
-					}
-					playersName.put(pc, s);
+			for(int i = 0; i < 6; i++) {
+				if(checkBoxes.get(i).isSelected()){
+					Model.PlayerColor pc = Model.PlayerColor.valueOf(checkBoxes.get(i).getText());
+					String name = textFields.get(i).getText();
+					playersName.put(pc, name);
 				}
-			}*/
+			}
+			System.out.print(playersName);
 			_parent.actionPerformed(new ChangeViewEvent(this, 200, "", ViewType.GAME));
 		}
 	}
