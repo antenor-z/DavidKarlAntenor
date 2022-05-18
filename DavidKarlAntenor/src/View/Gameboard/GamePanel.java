e33affc78755c7abe7ed2d054448551488195637
karlvignon <karl.vignon@epitech.eu>
2022-05-18 16:44:55 -0300

    small updates

--- DavidKarlAntenor/src/View/Gameboard/GamePanel.java
+++ DavidKarlAntenor/src/View/Gameboard/GamePanel.java
@@ -3,12 +3,21 @@ package View.Gameboard;
 import Model.Event.ChangeViewEvent;
 import Model.Event.ViewType;
 import Model.GameSettings;
+import View.Exception.ViewException;
 import View.MyPanel;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class GamePanel extends MyPanel {
     GameSettings settings = GameSettings.getInstance();
@@ -19,12 +28,51 @@ public class GamePanel extends MyPanel {
     JButton pauseButton = new JButton("Pause");
     JButton quiteButton = new JButton("Quit");
 
+    private int dice1Value = 1, dice2Value = 1;
+
     public GamePanel(CardLayout cl, JPanel panelCont, ActionListener controller) {
         super(cl, panelCont, controller);
         _initFrame();
         _setLeftPannel();
         _setRightPanel();
         _setEvents();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        _drawDices(g);
+        _drawCard(g);
+    }
+
+    private void _drawDices(Graphics g) {
+        String str1 = "./img/dados/die_face_" + dice1Value + ".png";
+        String str2 = "./img/dados/die_face_" + dice2Value + ".png";
+        Image imgDice1 = null;
+        Image imgDice2 = null;
+        try {
+            imgDice1= ImageIO.read(new File(str1));
+            imgDice2=ImageIO.read(new File(str2));
+        }
+        catch(IOException e2) {
+            System.out.println(e2.getMessage());
+            System.exit(1);
+        }
+        g.drawImage(imgDice1, 0, 100, 100, 100, null);
+        g.drawImage(imgDice2, 0, 250, 100, 100, null);
+    }
+
+    private void _drawCard(Graphics g) {
+        String str = "./img/sorteReves/chance" + 30 + ".png";
+        Image i = null;
+        try {
+            i=ImageIO.read(new File(str));
+        }
+        catch(IOException e2) {
+            System.out.println(e2.getMessage());
+            System.exit(1);
+        }
+        g.drawImage(i, settings.getGetMaxWidth() - 250, 400, null);
     }
 
     private void _setEvents() {
