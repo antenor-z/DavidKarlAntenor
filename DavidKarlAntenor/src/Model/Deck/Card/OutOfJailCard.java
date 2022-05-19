009ced00d3743ede9ce66dfd575af133e49da0ac
arm <arm@stargate>
2022-05-18 21:30:08 -0300

    implement owner

--- DavidKarlAntenor/src/Model/Deck/Card/OutOfJailCard.java
+++ DavidKarlAntenor/src/Model/Deck/Card/OutOfJailCard.java
@@ -1,8 +1,19 @@
 package Model.Deck.Card;
 
 public class OutOfJailCard extends ACard implements IKeptable {
+	private Model.Player.Player owner;
     public OutOfJailCard(String description, String name) {
         super(description, name);
         _type = CardType.OUT_OF_JAIL;
+        owner = null;
+    }
+    public void pick(Model.Player.Player p) {
+    	owner = p;
+    }
+    public Model.Player.Player getOwner() {
+    	return owner;
+    }
+    public void use() {
+    	owner = null;
     }
 }
