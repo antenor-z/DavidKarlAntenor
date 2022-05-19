5559eca3eb6a8cd13cab6450afbdf8df61abe8fe
arm <arm@stargate>
2022-05-18 21:29:23 -0300

    implement getOut

--- DavidKarlAntenor/src/Model/gameboard/Prision.java
+++ DavidKarlAntenor/src/Model/gameboard/Prision.java
@@ -1,7 +1,14 @@
 package Model.gameboard;
 
+import Model.Player.Player;
+
 public class Prision extends Tile {
 	void print(){
 		System.out.println("I'm a Prision tile");
+	}
+	void getOut(Player player, int diceNumber1, int diceNumber2) {
+		if(diceNumber1 == diceNumber2) {
+			player.goFoward(diceNumber1 + diceNumber2);
+		}
 	}
 }
