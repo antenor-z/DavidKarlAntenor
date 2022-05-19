988a85b983861002b9761d5f525f34d7bd658e3e
karlvignon <karl.vignon@epitech.eu>
2022-05-19 11:46:48 -0300

    fill de pause panel

--- DavidKarlAntenor/src/View/Pause/PausePanel.java
+++ DavidKarlAntenor/src/View/Pause/PausePanel.java
@@ -15,10 +15,16 @@ public class PausePanel extends MyPanel {
     JButton closeBtn = new JButton("close");
     GameListPanel gamesListPanel = null;
 
+    JPanel buttonsGrid = new JPanel();
+    JButton saveBtn = new JButton("Save");
+    JButton loadBtn = new JButton("Load");
+    JButton resumeBtn = new JButton("Resume");
+    JButton quiteBtn = new JButton("Quite");
+
     public PausePanel(CardLayout cl, JPanel panelCont, ActionListener controller) throws HeadlessException {
         super(cl, panelCont, controller);
         _initPanel();
-        _setCloseButtonEvent();
+        _setEvents();
     }
 
     private void _initPanel() {
@@ -26,6 +32,31 @@ public class PausePanel extends MyPanel {
         gamesListPanel = new GameListPanel(_cl, _panelCont, _controller);
         add(gamesListPanel);
         closeBtn.setPreferredSize(new Dimension(1200, 30));
+        _setButtonsGrid();
+    }
+
+    private void _setButtonsGrid() {
+        buttonsGrid.setLayout(new GridLayout(2, 2));
+        buttonsGrid.setPreferredSize(new Dimension(1200, 115));
+        buttonsGrid.add(saveBtn);
+        buttonsGrid.add(loadBtn);
+        buttonsGrid.add(resumeBtn);
+        buttonsGrid.add(quiteBtn);
+        add(buttonsGrid);
+    }
+
+    private void _setEvents() {
+        _setCloseButtonEvent();
+        _setResumeButtonEvent();
+    }
+
+    private void _setResumeButtonEvent() {
+        resumeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                _cl.show(_panelCont, "1");
+            }
+        });
     }
 
     private void _setCloseButtonEvent() {
